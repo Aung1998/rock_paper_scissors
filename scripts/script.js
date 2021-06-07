ddbbb718c046ea10d9d658a5e0b36fa2843d0dd4
Aung Myat Thu <aungmyatthu1998@gmail.com>
2021-06-07 11:46:59 +0630

    Added scores to HTML

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,15 @@ const HANDS = ["rock", "paper", "scissors"];
 let human_points = 0;
 let computer_points = 0;
 
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+const human_result = document.getElementById("human_point")
+const computer_result = document.getElementById("computer_point")
+
+result = document.createElement("div");
+
 function computerPlay() {
   return HANDS[Math.floor(Math.random() * HANDS.length)];
 }
@@ -35,20 +44,34 @@ function playGame(value) {
   } else {
     console.log("It's a Draw!");
   }
+  updateResult();
   decidewin();
 }
 
 function reset() {
   human_points = 0;
   computer_points = 0;
+  updateResult();
+  btn_disabled(false);
+}
+
+function updateResult(){
+  human_result.textContent = "Human Point: " + human_points;
+  computer_result.textContent = "Computer Point: " + computer_points;
+}
+
+function btn_disabled(bool){
+  rock.disabled = bool;
+  paper.disabled = bool;
+  scissors.disabled = bool;
 }
 
 function decidewin() {
   if (human_points == 5) {
-    console.log("Human win the game!");
-    reset();
+    btn_disabled(true);
+    result.textContent = "Human Win The Game!"
   } else if (computer_points == 5) {
-    console.log("Computer win the game!");
-    reset();
+    btn_disabled(true);
+    result.textContent = "Computer Win The Game!"
   }
 }
